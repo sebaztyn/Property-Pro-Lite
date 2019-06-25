4dec8ebbf489df75958563e45af342feab5ca699
Chima <sebastinechima@gmail.com>
2019-06-25 16:29:09 +0100

    implement update-property-details endpoint

--- Server/Routes/property.routes.js
+++ Server/Routes/property.routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { cloudinaryConfig } from '../config/cloudinaryConfig';
 import { multerUploads } from '../middleware/multer';
-import { postAdvert } from '../controllers/property.controller';
+import { postAdvert, updateAdvert } from '../controllers/property.controller';
 import { checkToken } from '../middleware/tokenHandler';
 import { propertyValidator } from '../middleware/validator';
 
 const router = Router();
 
 router.post('/', checkToken, cloudinaryConfig, multerUploads, propertyValidator, postAdvert);
+router.patch('/:propertyId', checkToken, cloudinaryConfig, multerUploads, propertyValidator, updateAdvert);
 
 
 export default router;
--- Server/controllers/property.controller.js
+++ Server/controllers/property.controller.js
@@ -30,5 +30,33 @@ const postAdvert = async (req, res) => {
     return serverError(res);
   }
 };
+const updateAdvert = async (req, res) => {
+  try {
+    let image_url;
+    if (req.file) {
+      const fileUrl = await imageUpload(req);
+      if (!fileUrl) image_url = 'https://dummytesturl.com';
+      else image_url = fileUrl;
+    }
+    const propId = Number(req.params.propertyId);
+    const propArray = propertyObj.findAllUsers();
+    const propToUpdate = propArray.filter(property => property.id === propId);
+    const propertyData = propToUpdate[0];
+    const propIndex = propArray.findIndex(property => property.id === propId);
+    const {
+      state, city, address, type, price
+    } = req.body;
+    propertyData.state = (propertyData.state === state) ? propertyData.state : state;
+    propertyData.price = (propertyData.price === price) ? propertyData.price : price;
+    propertyData.city = (propertyData.city === city) ? propertyData.city : city;
+    propertyData.address = (propertyData.address === address) ? propertyData.address : address;
+    propertyData.image_url = (propertyData.image_url === image_url) ? propertyData.image_url : image_url;
+    propertyData.type = (propertyData.type === type) ? propertyData.type : type;
+    propertyObj.updateAdvert(propertyData, propIndex);
+    return serverResponse(res, 201, 'status', 'success', 'data', propertyData);
+  } catch (err) {
+    return serverError(res);
+  }
+};
 
-export { postAdvert };
+export { postAdvert, updateAdvert };

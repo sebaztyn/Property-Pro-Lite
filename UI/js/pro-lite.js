17fdb913a2be8ece1834922b5244d7faed517043
Chima <sebastinechima@gmail.com>
2019-07-01 23:13:54 +0100

    chore(UI): refactor UI

--- UI/js/pro-lite.js
+++ UI/js/pro-lite.js
@@ -16,21 +16,22 @@ const thirdDivForm = document.querySelector('.third-div');
 const mainFormPage = document.querySelector('.main-form');
 const signUpButton = document.querySelector('#signup-button');
 const loginButton = document.querySelector('.login-submit-button');
-const propertyListContainer = document.querySelector('.property-display-container')
+const propertyListContainer = document.querySelector('.property-display-container');
 const mainFormSubmitButton = document.querySelector('.main-form-submit-button');
 const markSold = document.querySelector('.my-list-body');
 
 
-
-if (hamburgerIcon) hamburgerIcon.addEventListener('change', (event) => {
-  event.preventDefault();
-  if (hamburgerIcon.checked === true) {
-    navbar.style.display = 'flex';
-  }
-  else {
-    navbar.style.display = 'none';
-  }
-});
+if (hamburgerIcon) {
+  hamburgerIcon.addEventListener('change', (event) => {
+    event.preventDefault();
+    if (hamburgerIcon.checked === true) {
+      navbar.style.display = 'flex';
+    }
+    else {
+      navbar.style.display = 'none';
+    }
+  });
+}
 window.addEventListener('mouseup', (event) => {
   event.preventDefault();
   if (event.target !== hamburgerIcon && event.target.parentNode !== hamburgerIcon) {
@@ -40,112 +41,149 @@ window.addEventListener('mouseup', (event) => {
 
 });
 let nextScore = 0;
-if (previousButton) previousButton.addEventListener('click', (event) => {
-  if (nextScore === 2) {
-    thirdDivForm.style.left = '100vw';
-    secondDivForm.style.left = '0';
-    firstDivForm.style.left = '-100vw';
-    nextButton.style.display = 'inline';
-    mainFormSubmitButton.style.display = 'none';
-    thirdDivForm.style.opacity = '0';
-    secondDivForm.style.opacity = '1';
-    nextScore -= 1;
-  }
-  else if (nextScore === 1) {
-    thirdDivForm.style.left = '200vw';
-    secondDivForm.style.left = '100vw';
-    firstDivForm.style.left = '0';
-    nextScore -= 1;
-    previousButton.style.display = 'none';
-    secondDivForm.style.opacity = '0';
-    firstDivForm.style.opacity = '1';
-  }
-});
-if (nextButton) nextButton.addEventListener('click', (event) => {
-  if (nextScore === 0) {
-    firstDivForm.style.left = '-100vw';
-    secondDivForm.style.left = '0';
-    secondDivForm.style.opacity = '1';
-    thirdDivForm.style.left = '100vw';
-    nextScore += 1;
-    previousButton.style.display = 'inline';
-    firstDivForm.style.opacity = '0';
-  }
-  else if (nextScore === 1) {
-    firstDivForm.style.left = '-200vw';
-    secondDivForm.style.left = '-100vw';
-    thirdDivForm.style.left = '0';
-    mainFormSubmitButton.style.display = 'inline';
-    nextButton.style.display = 'none';
-    nextScore += 1;
-    secondDivForm.style.opacity = '0';
-    thirdDivForm.style.opacity = '1';
-  }
-});
-if (loginLink) loginLink.addEventListener('click', () => {
-  location.href = 'login.html';
-});
-if (myPropertyList) myPropertyList.addEventListener('click', () => {
-  location.href = 'my-list.html';
-});
-if (ulDashboard) ulDashboard.addEventListener('click', () => {
-  location.href = 'dashboard.html';
-});
-if (myProfile) myProfile.addEventListener('click', () => {
-  location.href = 'profile.html';
-});
-if (dashboardProfile) dashboardProfile.addEventListener('click', (event) => {
-  event.stopPropagation();
-  location.href = 'profile.html';
-});
-if (dashboardMyPropertyList) dashboardMyPropertyList.addEventListener('click', () => {
-  location.href = 'my-list.html';
-});
-if (signupLink) signupLink.addEventListener('click', () => {
-  location.href = 'signup.html';
-});
-if (listPropertyLink) listPropertyLink.addEventListener('click', () => {
-  location.href = 'listproperty.html';
-});
-if (markSold) markSold.addEventListener('click', (event) => {
-  if (event.target.matches('.listings>div:nth-child(2)>span:nth-child(3)')) {
-    event.target.parentElement.nextElementSibling.style.opacity = 1;
-  }
-});
-if (markSold) markSold.addEventListener('click', (event) => {
-  if (event.target.matches('.listings>div:nth-child(2)>span:nth-child(2)')) {
-    event.target.parentElement.parentElement.style.display = 'none';
-  }
-});
-
-if (signUpButton) signUpButton.addEventListener('click', () => {
-  localStorage.setItem('signup', 'Hello');
-  location.href = 'index.html';
-});
-if (loginButton) loginButton.addEventListener('click', () => {
-  localStorage.setItem('login', 'Heyyo');
-  location.href = 'index.html';
-});
-if (propertyListContainer) propertyListContainer.addEventListener('click', (event) => {
-  if (event.target.matches('.view-property')) {
-    const advert = Array.from(document.getElementsByClassName('display-rent'));
-    let targetElement = null;
-    for (let i = 0; i < advert.length; i++) {
-      if (event.target === advert[i] || event.target.parentElement === advert[i] || event.target.parentElement.parentElement === advert[i]) {
-        targetElement = advert[i];
-      }
+if (previousButton) {
+  previousButton.addEventListener('click', (event) => {
+    if (nextScore === 2) {
+      thirdDivForm.style.left = '100vw';
+      secondDivForm.style.left = '0';
+      firstDivForm.style.left = '-100vw';
+      nextButton.style.display = 'inline';
+      mainFormSubmitButton.style.display = 'none';
+      thirdDivForm.style.opacity = '0';
+      secondDivForm.style.opacity = '1';
+      nextScore -= 1;
     }
-    const values = [];
-    const imgSrc = targetElement.firstElementChild.firstElementChild.src;
-    const title = targetElement.firstElementChild.nextElementSibling.firstElementChild.textContent;
-    const propLocation = targetElement.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.textContent;
-    const price = targetElement.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.nextElementSibling.textContent;
-    values.push(imgSrc, title, propLocation, price);
-    const localString = values.join('  ')
-    localStorage.setItem('propInfo', localString);
-    location.href = 'property-detail.html';
-  }
-});
+    else if (nextScore === 1) {
+      thirdDivForm.style.left = '200vw';
+      secondDivForm.style.left = '100vw';
+      firstDivForm.style.left = '0';
+      nextScore -= 1;
+      previousButton.style.display = 'none';
+      secondDivForm.style.opacity = '0';
+      firstDivForm.style.opacity = '1';
+    }
+  });
+}
+if (nextButton) {
+  nextButton.addEventListener('click', (event) => {
+    if (nextScore === 0) {
+      firstDivForm.style.left = '-100vw';
+      secondDivForm.style.left = '0';
+      secondDivForm.style.opacity = '1';
+      thirdDivForm.style.left = '100vw';
+      nextScore += 1;
+      previousButton.style.display = 'inline';
+      firstDivForm.style.opacity = '0';
+    }
+    else if (nextScore === 1) {
+      firstDivForm.style.left = '-200vw';
+      secondDivForm.style.left = '-100vw';
+      thirdDivForm.style.left = '0';
+      mainFormSubmitButton.style.display = 'inline';
+      nextButton.style.display = 'none';
+      nextScore += 1;
+      secondDivForm.style.opacity = '0';
+      thirdDivForm.style.opacity = '1';
+    }
+  });
+}
+if (loginLink) {
+  loginLink.addEventListener('click', () => {
+    location.href = 'login.html';
+  });
+}
+if (myPropertyList) {
+  myPropertyList.addEventListener('click', () => {
+    location.href = 'my-list.html';
+  });
+}
+if (ulDashboard) {
+  ulDashboard.addEventListener('click', () => {
+    location.href = 'dashboard.html';
+  });
+}
+if (myProfile) {
+  myProfile.addEventListener('click', () => {
+    location.href = 'profile.html';
+  });
+}
+if (dashboardProfile) {
+  dashboardProfile.addEventListener('click', (event) => {
+    event.stopPropagation();
+    location.href = 'profile.html';
+  });
+}
+if (dashboardMyPropertyList) {
+  dashboardMyPropertyList.addEventListener('click', () => {
+    location.href = 'my-list.html';
+  });
+}
+if (signupLink) {
+  signupLink.addEventListener('click', () => {
+    location.href = 'signup.html';
+  });
+}
+if (listPropertyLink) {
+  listPropertyLink.addEventListener('click', () => {
+    location.href = 'listproperty.html';
+  });
+}
+if (markSold) {
+  markSold.addEventListener('click', (event) => {
+    if (event.target.matches('.mark-sold')) {
+      return event.target.parentElement.nextElementSibling.style.opacity = 1;
+    }
+  });
+}
+if (markSold) {
+  markSold.addEventListener('click', (event) => {
+    if (event.target.matches('.mark-sold>.fa-check-circle')) {
+      return event.target.parentElement.parentElement.nextElementSibling.style.opacity = 1;
+    }
+  });
+}
+if (markSold) {
+  markSold.addEventListener('click', (event) => {
+    if (event.target.matches('.listings>div:nth-child(2)>span:nth-child(2)')) {
+      event.target.parentElement.parentElement.style.display = 'none';
+    }
+  });
+}
+
+if (signUpButton) {
+  signUpButton.addEventListener('click', () => {
+    localStorage.setItem('signup', 'Hello');
+    location.href = 'index.html';
+  });
+}
+if (loginButton) {
+  loginButton.addEventListener('click', () => {
+    localStorage.setItem('login', 'Heyyo');
+    location.href = 'index.html';
+  });
+}
+if (propertyListContainer) {
+  propertyListContainer.addEventListener('click', (event) => {
+    if (event.target.matches('.view-property')) {
+      const advert = Array.from(document.getElementsByClassName('display-rent'));
+      let targetElement = null;
+      for (let i = 0; i < advert.length; i++) {
+        if (event.target === advert[i] || event.target.parentElement === advert[i] || event.target.parentElement.parentElement === advert[i]) {
+          targetElement = advert[i];
+        }
+      }
+      const values = [];
+      const imgSrc = targetElement.firstElementChild.firstElementChild.src;
+      const title = targetElement.firstElementChild.nextElementSibling.firstElementChild.textContent;
+      const propLocation = targetElement.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.textContent;
+      const price = targetElement.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.nextElementSibling.textContent;
+      values.push(imgSrc, title, propLocation, price);
+      const localString = values.join('  ');
+      localStorage.setItem('propInfo', localString);
+      location.href = 'property-detail.html';
+    }
+  });
+}
 if (document.querySelector('.property-information .image')) {
   const retrievedValues = localStorage.getItem('propInfo');
   const splitValues = retrievedValues.split('  ');
@@ -187,4 +225,9 @@ signoutOne.addEventListener('click', (event) => {
   localStorage.removeItem('signup');
   localStorage.removeItem('login');
   location.href = 'index.html';
-})
+});
+signoutTwo.addEventListener('click', (event) => {
+  localStorage.removeItem('signup');
+  localStorage.removeItem('login');
+  location.href = 'index.html';
+});
